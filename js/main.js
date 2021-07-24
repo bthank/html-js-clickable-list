8d65e3e33fb4e9fd8fdcf377b6bf16e88fef9a54
bthank <bthankachan@yahoo.com>
2021-07-24 18:42:30 -0400

    Added add new item functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // use listItems to hold all of the list elements
 var favCar = document.getElementById("fav-car");
+var addNewItemBtn = document.getElementById("add-new-item-btn");
+var itemList = document.getElementById("item-list");
 var listItems = document.getElementById("item-list").getElementsByTagName("li");
+var cnt = 0;
 
 for (i=0; i < listItems.length; i++) {
     listItems[i].addEventListener("click",activateItem);
@@ -8,4 +11,11 @@ for (i=0; i < listItems.length; i++) {
 
 function activateItem(){
     favCar.innerHTML = this.innerHTML;
+}
+
+addNewItemBtn.addEventListener("click",createNewItem);
+
+function createNewItem(){
+    cnt++;
+    itemList.innerHTML += "<li>New Car " + cnt + "</li>";
 }
