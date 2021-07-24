373d02069de165c678d6fbf39d9243dcd5be7d69
bthank <bthankachan@yahoo.com>
2021-07-24 18:16:30 -0400

    Added event listener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var pageHeadline = document.getElementById("page-headline");
+// use listItems to hold all of the list elements
+var listItems = document.getElementById("item-list").getElementsByTagName("li");
+
+for (i=0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click",activateItem);
+}
+
+function activateItem(){
+    alert("Click detected");
+}
